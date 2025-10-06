dcf774c510e435ba66ff5b818a3b649e96664249
Prathamesh Pendal <prathameshpendal71@gmail.com>
2025-10-07 01:06:53 +0530

    SQL file added

---
+++ sample.sql
@@ -0,0 +1,14 @@
+use attendance_payroll_db;
+show tables;
+
+select * from attendance;
+
+select * from public_holidays;
+
+select * from company_settings;
+
+select * from leaves;
+
+select * from users;
+
+select * from user_roles; 
