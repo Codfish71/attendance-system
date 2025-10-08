20d2b3542a9b485092fc661b2072b4c8a4c46808
Prathamesh Pendal <prathameshpendal71@gmail.com>
2025-10-09 03:18:32 +0530

    CORS host updated

--- src/main/java/com/geeksmetrics/attendance_mgmt/config/WebConfig.java
+++ src/main/java/com/geeksmetrics/attendance_mgmt/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://69.62.84.52:80")
+                .allowedOrigins("http://69.62.84.52", "http://srv1053724.hstgr.cloud")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
