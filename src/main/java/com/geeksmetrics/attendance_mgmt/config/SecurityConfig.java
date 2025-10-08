ba7a8a0a99df998ad1772e16433e3d425f8fe738
Prathamesh Pendal <prathameshpendal71@gmail.com>
2025-10-09 03:10:26 +0530

    CORS host updated

--- src/main/java/com/geeksmetrics/attendance_mgmt/config/SecurityConfig.java
+++ src/main/java/com/geeksmetrics/attendance_mgmt/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://69.62.84.52"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
